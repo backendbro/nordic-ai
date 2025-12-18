4e272e05053049c8ebacefcee69b7819b7d750be
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-12-18 01:40:43 +0100

    Fogot to add use client

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Content, DateField, isFilled } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
