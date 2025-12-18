6bb8543ca020f895c0170f2beed7f3c310f021dc
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-12-19 00:44:22 +0100

    o

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { Content, DateField, isFilled } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
