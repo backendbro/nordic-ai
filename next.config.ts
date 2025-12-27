034094e8783e9ef7070b52bfb3e1d5a45bcc164e
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-12-27 20:52:38 +0100

    Got rid of the suspense

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: [
+      "three",
+      "@react-three/fiber",
+      "@react-three/drei",
+    ],
+  },
 };
 
 export default nextConfig;
