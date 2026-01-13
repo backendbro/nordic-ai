7bb58415a97dbc8d8ceaf4ef7fdb76f276316911
backendbro <ukagha.nzubechukwu@yahoo.com>
2026-01-13 18:03:12 +0100

    Addedd an icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: settings.data.meta_title,
     description: settings.data.meta_description,
+    icons: {
+      icon: "/icon.png",
+    },
   };
 }
 
